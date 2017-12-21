2581440c5986f8dad98295a07a32e4480de5fc66
arielkayne <Ariel@Lavi-Mini.local>
2017-12-21 00:24:24 -0600

    more sql work

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
 -- In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Beefinator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Howdy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Skinny Burger', false);
+
