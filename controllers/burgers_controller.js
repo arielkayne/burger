de92e4759e32d97cd655d6cf93ecd37f099609e7
arielkayne <Ariel@Lavi-Mini.local>
2017-12-21 01:06:56 -0600

    most of the work is done...minus the orm file and views

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+var express = require('express');
+
+
+// 4. Create the `router` for the app, and export the `router` at the end of your file.
